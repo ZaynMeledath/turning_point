1d20c02c2e20bc093f729c8707ce4ccab629d8dd
unknown <zaynmeledath@gmail.com>
2024-02-05 14:54:36 +0530

    some minor UI changes

--- lib/view/contest/segments/banner_segment.dart
+++ lib/view/contest/segments/banner_segment.dart
@@ -3,89 +3,145 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:turning_point/helper/screen_size.dart';
 
 Widget bannerSegment() {
-  return Column(
+  return Stack(
     children: [
-      Container(
-        width: double.infinity,
-        height: screenSize.height * .19,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Stack(
-          children: [
-            Padding(
-              padding:
-                  EdgeInsets.symmetric(horizontal: screenSize.width * .061),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'PLAY AND EARN',
-                    style: GoogleFonts.poppins(
-                      fontSize: screenSize.width * .051,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    'Earn Prize by participating in\nContests',
-                    style: GoogleFonts.poppins(
-                      color: const Color.fromRGBO(0, 90, 143, 1),
-                      fontSize: screenSize.width * .038,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Container(
-                    width: screenSize.width * .194,
-                    height: screenSize.width * .066,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: const Color.fromRGBO(0, 99, 255, 1),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Play Now',
-                        style: GoogleFonts.poppins(
-                          color: Colors.white,
-                          fontSize: screenSize.width * .03,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+      Positioned(
+        top: 4,
+        left: screenSize.width * .02,
+        child: Container(
+          width: screenSize.width * .33,
+          height: screenSize.width * .33,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, -1),
+                color: Color.fromRGBO(72, 72, 72, 0.04),
               ),
-            ),
-            Positioned(
-              right: -4,
-              bottom: 0,
-              child: Image.asset(
-                'assets/images/contest_banner_image.png',
-                width: screenSize.width * .5,
+              BoxShadow(
+                offset: Offset(0, 5),
+                color: Color.fromRGBO(177, 177, 177, 0.25),
+                blurStyle: BlurStyle.inner,
               ),
-            )
-          ],
+              BoxShadow(
+                color: Color.fromRGBO(247, 247, 247, 1),
+              ),
+            ],
+          ),
         ),
       ),
-      SizedBox(height: screenSize.height * .01),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      Positioned(
+        bottom: 5,
+        right: -screenSize.height * .06,
+        child: Container(
+          width: screenSize.height * .2,
+          height: screenSize.height * .2,
+          decoration: const BoxDecoration(
+            color: Color.fromRGBO(247, 247, 247, 1),
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 1),
+                color: Color.fromRGBO(72, 72, 72, 0.04),
+              ),
+              BoxShadow(
+                offset: Offset(-5, -5),
+                color: Color.fromRGBO(177, 177, 177, 0.25),
+                blurStyle: BlurStyle.inner,
+              ),
+              BoxShadow(
+                color: Color.fromRGBO(247, 247, 247, 1),
+              ),
+            ],
+          ),
+        ),
+      ),
+      Column(
         children: [
-          Text(
-            'Available Points : ',
-            style: GoogleFonts.roboto(
-              color: const Color.fromRGBO(0, 99, 255, 1),
-              fontSize: screenSize.width * .03,
-              fontWeight: FontWeight.w500,
+          Container(
+            width: double.infinity,
+            height: screenSize.height * .19,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: screenSize.width * .065),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'PLAY AND EARN',
+                        style: GoogleFonts.poppins(
+                          fontSize: screenSize.width * .051,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(height: screenSize.height * .008),
+                      Text(
+                        'Earn Prize by participating in\nContests',
+                        style: GoogleFonts.poppins(
+                          color: const Color.fromRGBO(0, 90, 143, 1),
+                          fontSize: screenSize.width * .035,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      // Container(
+                      //   width: screenSize.width * .194,
+                      //   height: screenSize.width * .066,
+                      //   decoration: BoxDecoration(
+                      //     borderRadius: BorderRadius.circular(5),
+                      //     color: const Color.fromRGBO(0, 99, 255, 1),
+                      //   ),
+                      //   child: Center(
+                      //     child: Text(
+                      //       'Play Now',
+                      //       style: GoogleFonts.poppins(
+                      //         color: Colors.white,
+                      //         fontSize: screenSize.width * .03,
+                      //         fontWeight: FontWeight.w500,
+                      //       ),
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: -4,
+                  bottom: 0,
+                  child: Image.asset(
+                    'assets/images/contest_banner_image.png',
+                    width: screenSize.width * .5,
+                  ),
+                )
+              ],
             ),
           ),
-          Text(
-            '10000',
-            style: GoogleFonts.roboto(
-              color: const Color.fromRGBO(255, 152, 0, 1),
-              fontSize: screenSize.width * .032,
-              fontWeight: FontWeight.w500,
-            ),
+          SizedBox(height: screenSize.height * .01),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Available Points : ',
+                style: GoogleFonts.roboto(
+                  color: const Color.fromRGBO(0, 99, 255, 1),
+                  fontSize: screenSize.width * .03,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                '10000',
+                style: GoogleFonts.roboto(
+                  color: const Color.fromRGBO(255, 152, 0, 1),
+                  fontSize: screenSize.width * .032,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
           ),
         ],
       ),
