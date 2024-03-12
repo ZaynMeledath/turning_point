85b935cb172302772653e283e41e17e57927fdeb
unknown <zaynmeledath@gmail.com>
2024-03-12 17:51:34 +0530

    Scanner Screen UI change

--- lib/view/scanner/scanner_screen.dart
+++ lib/view/scanner/scanner_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:turning_point/bloc/home/home_bloc.dart';
 import 'package:turning_point/bloc/preload/preload_bloc.dart';
@@ -25,12 +26,16 @@ class ScannerScreen extends StatefulWidget {
 }
 
 class _ScannerScreenState extends State<ScannerScreen> {
+  late final TextEditingController couponController;
+  bool shouldScan = true;
+
   @override
   void initState() {
     super.initState();
     if (preloadBloc.state.controllers.isNotEmpty) {
       preloadBloc.pauseCurrentController();
     }
+    couponController = TextEditingController();
     getLocation();
   }
 
@@ -39,6 +44,12 @@ class _ScannerScreenState extends State<ScannerScreen> {
     log('##############LOCATION###########');
     log(location.latitude.toString());
     log(location.longitude.toString());
+  }
+
+  @override
+  void dispose() {
+    couponController.dispose();
+    super.dispose();
   }
 
   @override
@@ -102,77 +113,150 @@ class _ScannerScreenState extends State<ScannerScreen> {
         },
         builder: (context, state) {
           return SafeArea(
-            child: Column(
-              children: [
-                SizedBox(height: screenSize.height * .06),
-                Container(
-                  width: double.maxFinite,
-                  height: screenSize.height * .18,
-                  margin:
-                      EdgeInsets.symmetric(horizontal: screenSize.width * .036),
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(246, 246, 246, 1),
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Scan QR Code',
-                        style: GoogleFonts.roboto(
-                          fontSize: screenSize.width * .06,
-                          fontWeight: FontWeight.bold,
-                        ),
+            child: SingleChildScrollView(
+              reverse: true,
+              child: Column(
+                children: [
+                  SizedBox(height: screenSize.height * .06),
+                  Container(
+                    width: double.maxFinite,
+                    height: screenSize.height * .18,
+                    margin: EdgeInsets.symmetric(
+                        horizontal: screenSize.width * .036),
+                    decoration: BoxDecoration(
+                      // color: const Color.fromRGBO(246, 246, 246, 1),
+                      gradient: const LinearGradient(
+                        colors: [
+                          Color.fromRGBO(246, 246, 246, 1),
+                          Color.fromRGBO(250, 250, 250, 1),
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
                       ),
-                      SizedBox(height: screenSize.height * .01),
-                      Text(
-                        'Scan the QR to obtain points',
-                        style: GoogleFonts.roboto(
-                          fontSize: screenSize.width * .036,
-                          fontWeight: FontWeight.bold,
-                          color: const Color.fromRGBO(95, 95, 95, 1),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Scan QR Code',
+                          style: GoogleFonts.roboto(
+                            fontSize: screenSize.width * .06,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
+                        SizedBox(height: screenSize.height * .01),
+                        Text(
+                          'Scan QR Code to obtain points',
+                          style: GoogleFonts.roboto(
+                            fontSize: screenSize.width * .036,
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromRGBO(95, 95, 95, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: screenSize.height * .03),
+                  GestureDetector(
+                    onTap: () => scannerBloc.scanCoupon(),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Lottie.asset(
+                          'assets/lottie/scanner_screen_animation.json',
+                          width: screenSize.width * .65,
+                        ),
+                        Lottie.asset(
+                          'assets/lottie/scanner_screen_animation2.json',
+                          width: screenSize.width * .65,
+                        ),
+                        Image.asset(
+                          'assets/images/scanner_screen_image.png',
+                          width: screenSize.width * .45,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: screenSize.height * .03),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenSize.width * .08),
+                    child: TextFormField(
+                      controller: couponController,
+                      onChanged: (value) {
+                        if (value.isNotEmpty) {
+                          setState(() {
+                            shouldScan = false;
+                          });
+                        }
+                      },
+                      style: GoogleFonts.roboto(
+                        fontSize: screenSize.width * .031,
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: screenSize.height * .07),
-                GestureDetector(
-                  onTap: () => scannerBloc.scanCoupon(),
-                  child: Image.asset(
-                    'assets/images/scanner_screen_image.png',
-                    width: screenSize.width * .6,
-                  ),
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: GestureDetector(
-                      onTap: () => scannerBloc.scanCoupon(),
-                      child: Container(
-                        width: screenSize.width * .35,
-                        height: screenSize.height * .055,
-                        margin:
-                            EdgeInsets.only(bottom: screenSize.height * .03),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(0, 99, 255, 1),
-                          borderRadius: BorderRadius.circular(14),
+                      decoration: InputDecoration(
+                        hintText: 'Enter Coupon Code Manually',
+                        hintStyle: GoogleFonts.roboto(
+                          fontSize: screenSize.width * .031,
                         ),
-                        child: Center(
-                          child: Text(
-                            'Scan',
-                            style: GoogleFonts.roboto(
-                              fontSize: screenSize.width * .051,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white,
-                            ),
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: screenSize.width * .04,
+                        ),
+                        filled: true,
+                        fillColor: const Color.fromRGBO(242, 242, 242, 1),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          borderSide: const BorderSide(
+                            color: Colors.transparent,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6),
+                          borderSide: const BorderSide(
+                            color: Colors.blue,
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: screenSize.height * .07),
+                  GestureDetector(
+                    onTap: () {
+                      if (shouldScan) {
+                        scannerBloc.scanCoupon();
+                      } else {
+                        scannerBloc.add(
+                          ScannerCodeDetectEvent(
+                            couponId: couponController.text,
+                          ),
+                        );
+                      }
+                    },
+                    child: Container(
+                      width: screenSize.width * .35,
+                      height: screenSize.height * .055,
+                      margin: EdgeInsets.only(bottom: screenSize.height * .03),
+                      decoration: BoxDecoration(
+                        color: const Color.fromRGBO(0, 99, 255, 1),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      child: Center(
+                        child: Text(
+                          shouldScan ? 'Scan' : 'Submit',
+                          style: GoogleFonts.roboto(
+                            fontSize: screenSize.width * .051,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
